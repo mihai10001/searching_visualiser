0c825f83504f5a971d3400d49aa449a6a610d08a
mihai10001 <rvivits@gmail.com>
2021-09-23 11:49:12 +0300

    enums for data structure types and possible 2D Grid values

---
+++ searching-visualiser/src/app/searching-data-structures.ts
@@ -0,0 +1,14 @@
+export enum InputDataStructureTypes {
+    '2DGrid' = '2DGrid',
+    '3DGrid' = '3DGrid',
+    'BinaryTree' = 'BinaryTree',
+    // 'Graph'
+}
+
+export enum Possible2DGridValues {
+    Start = 0,
+    Visited = 1,
+    NotVisitied = 2,
+    Goal = 3,
+    Heuristic
+}
