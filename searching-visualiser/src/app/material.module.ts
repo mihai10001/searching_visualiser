241b99f261e95ce3527e5d29a6fb0c685c4afbe1
mihai10001 <rvivits@gmail.com>
2021-09-27 11:58:36 +0300

    missing material module imports

--- searching-visualiser/src/app/material.module.ts
+++ searching-visualiser/src/app/material.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 @NgModule({
   imports: [
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule
   ],
   exports: [
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule
   ]
 })
 export class MaterialModule {}
