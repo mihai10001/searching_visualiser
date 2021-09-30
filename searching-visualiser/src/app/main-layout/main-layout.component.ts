8ad3da555e029112d7eaa40d3d8787774dfe787c
mihai10001 <rvivits@gmail.com>
2021-09-30 17:56:21 +0300

    create main-layout to render needed components (similar to sorting-visualiser)

--- searching-visualiser/src/app/main-layout/main-layout.component.ts
+++ searching-visualiser/src/app/main-layout/main-layout.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener, OnInit, AfterViewInit } from '@angular/core';
+import { ViewChildren, ElementRef, QueryList } from '@angular/core';
+
+import { SearchingFunctions, SearchingFunctionsObjectType } from '../searching-functions';
+import { SettingsGeneralService } from './services/settings-general.service';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.css']
 })
-export class MainLayoutComponent implements OnInit {
+export class MainLayoutComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChildren('searchingCanvases', {read: ElementRef})
+  searchingCanvases!: QueryList<ElementRef>;
 
-  ngOnInit(): void {
+  searchingCanvasWidth: number = 0;
+  searchingCanvasHeight: number = 200;
+  searchingFunctions: SearchingFunctionsObjectType | any = SearchingFunctions;
+
+  selectedDataStructureKey: string = '';
+  selectedSearchingFunctionKeys: string[] = [];
+
+  constructor(
+    private changeDetector: ChangeDetectorRef,
+    private settingsGeneralService: SettingsGeneralService,
+  ) { }
+
+  ngOnInit() {
+    this.settingsGeneralService.selectedDataStructureKey.subscribe(key =>
+      this.selectedDataStructureKey = key
+    );
+    this.settingsGeneralService.selectedSearchingFunctionKeys.subscribe(keys =>
+      this.selectedSearchingFunctionKeys = keys
+    );
   }
 
+  ngAfterViewInit() {
+    this.searchingCanvases.changes.subscribe(() => { this.refreshCanvas(); this.detectChanges(); } );
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.refreshCanvas();
+  }
+
+  refreshCanvas() {
+    this.searchingCanvasWidth = this.searchingCanvases.length > 0 ? this.searchingCanvases.first.nativeElement.offsetWidth - 10 : 0;
+  }
+
+  detectChanges() {
+    this.changeDetector.detectChanges();
+  }
 }
